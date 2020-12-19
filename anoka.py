9f2d3b77f65aa111335966748fd046aec8944f13
Programmer-666 <suharslan@protonmail.ch>
2020-12-19 16:36:20 +0300

    a new page

--- anoka.py
+++
@@ -1,3 +0,0 @@
-import anoka_module
-if __name__ == "__main__":
-    anoka_module.mainClass()
--- anoka_module/session_control_module.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-class Anoka_Session_Control_Module:
-    def __init__(self):
-        pass
